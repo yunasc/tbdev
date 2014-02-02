5d3a3564b538e34232eadc6c3b07a336fecac702
Yuna Scatari <stirol@gmail.com>
2014-02-02 10:31:33 +0000

    mysql_numrows -> mysql_num_rows
    P.S: Пора переходить на DBAL

--- suggest.php
+++ suggest.php
@@ -11,12 +11,12 @@ if (strlen($_GET['q']) > 3) {
 	$q = str_replace(" ",".",sqlesc("%".$_GET['q']."%"));
 	$q2 = str_replace("."," ",sqlesc("%".$_GET['q']."%"));
 	$result = mysql_query("SELECT name FROM torrents WHERE name LIKE {$q} OR name LIKE {$q2} ORDER BY id DESC LIMIT 0,10;");
-	if (mysql_numrows($result) > 0) {
-		for ($i = 0; $i < mysql_numrows($result); $i++) {
+	if (mysql_num_rows($result) > 0) {
+		for ($i = 0; $i < mysql_num_rows($result); $i++) {
 			$name = mysql_result($result,$i,"name");
 			$name = trim(str_replace("\t","",$name));
 			print $name;
-			if ($i != mysql_numrows($result)-1) {
+			if ($i != mysql_num_rows($result)-1) {
 				print "\r\n";
 			}
 		}
