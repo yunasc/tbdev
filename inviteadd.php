2e68a7ff5711c023c6f1cbd59282c6115d0592f3
mft39 <mft39anton@gmail.com>
2014-02-24 11:07:29 -0800

    Update inviteadd.php
    Есть стандартный inviteadd.php
    я у путиа нашёл и поправил, где можно добавлять определённым классам,
    отнимать и удалять инвайты у всех, там ещё фича, выбор с оповещением и без

--- inviteadd.php
+++ inviteadd.php
@@ -1,69 +1,103 @@
-<?
+<?php
+/*+----------------------------------------------------+*/
+/*| made by putyn @ tbdev 31/05/2009 updated 30/09/2009|*/
+/*+----------------------------------------------------+*/
 
-/*
-// +--------------------------------------------------------------------------+
-// | Project:    TBDevYSE - TBDev Yuna Scatari Edition                        |
-// +--------------------------------------------------------------------------+
-// | This file is part of TBDevYSE. TBDevYSE is based on TBDev,               |
-// | originally by RedBeard of TorrentBits, extensively modified by           |
-// | Gartenzwerg.                                                             |
-// |                                                                          |
-// | TBDevYSE is free software; you can redistribute it and/or modify         |
-// | it under the terms of the GNU General Public License as published by     |
-// | the Free Software Foundation; either version 2 of the License, or        |
-// | (at your option) any later version.                                      |
-// |                                                                          |
-// | TBDevYSE is distributed in the hope that it will be useful,              |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-// | GNU General Public License for more details.                             |
-// |                                                                          |
-// | You should have received a copy of the GNU General Public License        |
-// | along with TBDevYSE; if not, write to the Free Software Foundation,      |
-// | Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA            |
-// +--------------------------------------------------------------------------+
-// |                                               Do not remove above lines! |
-// +--------------------------------------------------------------------------+
-*/
+require_once "include/bittorrent.php";
 
-require_once("include/bittorrent.php");
 dbconn();
 loggedinorreturn();
 
-if (get_user_class() < UC_SYSOP)
-    stderr("Error", "Access denied.");
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    if ($_POST["username"] == "" || $_POST["invites"] == "" || $_POST["invites"] == "")
-        stderr("Error", "Missing form data.");
-
-    $username = sqlesc($_POST["username"], true);
-    $invites = sqlesc($_POST["invites"]);
-
-    $res = sql_query("SELECT id FROM users WHERE username = $username");
-    $arr = mysql_fetch_row($res);
-
-    if (!$arr)
-        stderr("Error", "Unable to update account.");
-
-    sql_query("UPDATE users SET invites=$invites WHERE username = $username") or sqlerr(__FILE__, __LINE__);
-
-    header("Location: $DEFAULTBASEURL/userdetails.php?id=$arr[0]");
-    die;
-}
-
-stdhead("Update Users Invite Amounts");
-
-?>
-
-<h1>Update Users Invite Amounts</h1>
-<form method="post">
-	<table border=1 cellspacing=0 cellpadding=5>
-		<tr><td class=rowhead>User name</td><td><input type=text name=username size=40></td></tr>
-		<tr><td class=rowhead>Invites</td><td><input type=uploaded name=invites size=5></td></tr>
-		<tr><td colspan=2 align=center><input type=submit value="Okay" class=btn></td></tr>
+if (get_user_class() < UC_ADMINISTRATOR)
+	stderr("Error", "Access denied.");
+	
+	//dont forget to edit this 
+	$maxclass = UC_SYSOP;
+	$firstclass = UC_USER;
+	$use_subject = true;
+	
+	function mkpositive($n)
+	{
+		return strstr((string)$n,"-") ? 0 : $n ; // this will return 0 for negative numbers 
+	}
+	
+	if ($HTTP_SERVER_VARS["REQUEST_METHOD"] == "POST")
+	{
+		$classes = isset($_POST["classes"]) ? $_POST["classes"] : "";
+		$all = ($classes[0] == 255 ? true : false );
+		
+		if(empty($classes) || sizeof($classes) == 0 )
+			stderr("Err","You need at least one class selected");
+		$a_do = array("add","remove","remove_all");
+		$do = isset($_POST["do"]) && in_array($_POST["do"],$a_do) ? $_POST["do"] : "";
+		if(empty($do))
+			stderr("Err","wtf are you trying to do ");
+			
+		$invites = isset($_POST["invites"]) ? 0+$_POST["invites"] : 0;
+		if($invites == 0 && ($do == "add" || $do == "remove"))
+			stderr("Err","You can't remove/add 0");
+			
+		$sendpm = isset($_POST["pm"]) && $_POST["pm"] == "yes" ? true : false;
+		
+		$pms = array();
+		$users = array();
+		//select the users
+		$q = mysql_query("SELECT id,invites,username FROM users ".($all ? "" : "WHERE class in (".join(",",$classes).")" )." ORDER BY id desc ") or sqlerr(__FILE__, __LINE__);
+		if(mysql_num_rows($q) == 0)
+		stderr("Sorry","There are no users in the class(es) you selected");
+			while($a = mysql_fetch_assoc($q))
+			{
+				$users[] = "(".$a["id"].", ".($do == "remove_all" ? 0 : ($do == "add" ? $a["invites"] + $invites : mkpositive($a["invites"] - $invites))) .")";
+				if($sendpm)
+				{
+$subject = sqlesc($do == "remove_all" && $do == "remove" ?  "" : "");
+$body = sqlesc("".$a['username']."\n \n ". ($do == "remove_all" ?  "Все приглашения были отняты у Вашего класса пользователей" : ($do == "add" ? "$invites".($invites > 1 ? " приглашения были добавлены " : " приглашение было добавлено ")." Вашему классу пользователей" : "$invites".($invites > 1 ? " приглашения были отняты " : " приглашение было отнято ")."у Вашей группы пользователей")). " !\n \n Администрация ".$tracker_lang['site_name']." ");
+$pms[] = "(0,".$a['id'].",".sqlesc(get_date_time()).",$body ".($use_subject ? ",$subject" : "").")" ;
+				}
+			}
+			
+			if(sizeof($users) > 0)
+				$r = mysql_query("INSERT INTO users(id,invites) VALUES ".join(",",$users)." ON DUPLICATE key UPDATE invites=values(invites) ") or sqlerr(__FILE__, __LINE__);
+			if(sizeof($pms) > 0)
+				$r1 = mysql_query("INSERT INTO messages (sender, receiver, added, msg ".($use_subject ? ", subject" : "").") VALUES ".join(",",$pms)." ") or sqlerr(__FILE__, __LINE__);
+				
+			if($r && ($sendpm ? $r1 : true))
+			{
+				header("Refresh: 2; url=".$_SERVER["PHP_SELF"]);
+				stderr("Succes","Operation done!");
+			}
+			else
+				stderr("Error","Something was wrong");
+	}
+	$HTMLOUT ='';
+	$HTMLOUT .= "<form  action=\"".$_SERVER["PHP_SELF"]."\" method=\"post\">
+	<table width=\"500\" cellpadding=\"5\" cellspacing=\"0\" border=\"1\" align=\"center\">
+	  <tr>
+		<td valign=\"top\" align=\"right\">Classes</td>
+		<td width=\"100%\" align=\"left\" colspan=\"3\">";
+				$HTMLOUT .= "<label for=\"all\"><input type=\"checkbox\" name=\"classes[]\" value=\"255\" id=\"all\" />Всем классам</label><br/>\n";
+				for($i=$firstclass;$i<$maxclass+1; $i++ )
+				$HTMLOUT .= "<label for=\"c$i\"><input type=\"checkbox\" name=\"classes[]\" value=\"$i\" id=\"c$i\" />".get_user_class_name($i)." </label><br/>\n";
+	$HTMLOUT .= "</td>
+	  </tr>
+	  <tr>
+		<td valign=\"top\" align=\"center\" >Options</td>
+		<td valign=\"top\">Do
+		  <select name=\"do\" >
+			<option value=\"add\">add invites</option>
+			<option value=\"remove\">remove invites</option>
+			<option value=\"remove_all\">Remove all invites</option>
+		  </select></td>
+		<td>Invites <input type=\"text\" maxlength=\"2\" name=\"invites\" size=\"5\" />
+		</td>
+		<td >Send pm <select name=\"pm\" ><option value=\"no\">no</option><option value=\"yes\">yes</option></select></td></tr>
+		<tr><td colspan=\"4\" align=\"center\"><input type=\"submit\" value=\"Do!\" /></td></tr>
 	</table>
-</form>
-
-<? stdfoot(); ?>
+	</form>";
+	
+	stdhead("Add/Remove invites");
+        print($HTMLOUT);
+        stdfoot();
+?>
