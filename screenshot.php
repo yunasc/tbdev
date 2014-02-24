4073a1981b9d11a9e153b16365e9b9c73dc9c96e
yunasc <stirol@gmail.com>
2014-02-24 19:45:30 +0200

    [BUGFIX] thumbnail.php fix + reformat code of screenshot.php

--- screenshot.php
+++ screenshot.php
@@ -40,12 +40,12 @@ $img = null;
 $tmp = explode('.', $image_path);
 $ext = strtolower(end($tmp));
 if ($ext == 'jpg' || $ext == 'jpeg') {
-$img = @imagecreatefromjpeg($image_path);
+	$img = @imagecreatefromjpeg($image_path);
 } else if ($ext == 'png') {
-$img = @imagecreatefrompng($image_path);
+	$img = @imagecreatefrompng($image_path);
 # Only if your version of GD includes GIF support
 } else if ($ext == 'gif') {
-$img = @imagecreatefromgif($image_path);
+	$img = @imagecreatefromgif($image_path);
 }
 
 # If an image was successfully loaded, test the image for size
@@ -53,22 +53,22 @@ if ($img) {
 	# Get image size and scale ratio
 	$width = imagesx($img);
 	$height = imagesy($img);
-	$scale = min(MAX_WIDTH/$width, MAX_HEIGHT/$height);
+	$scale = min(MAX_WIDTH / $width, MAX_HEIGHT / $height);
 
 	# If the image is larger than the max shrink it
 	if ($scale < 1) {
 		if (($width > MAX_WIDTH) OR ($height > MAX_HEIGHT)) {
-			$new_width = floor($scale*$width);
-			$new_height = floor($scale*$height);
+			$new_width = floor($scale * $width);
+			$new_height = floor($scale * $height);
 			# Create a new temporary image
 			$tmp_img = imagecreatetruecolor($new_width, $new_height);
 			# Copy and resize old image into new image
 			imagecopyresampled($tmp_img, $img, 0, 0, 0, 0,
-			$new_width, $new_height, $width, $height);
+							   $new_width, $new_height, $width, $height);
 			imagedestroy($img);
 			$img = $tmp_img;
 		}
-	}else{
+	} else {
 		header("Location: $image_path");
 		exit;
 	}
@@ -77,10 +77,10 @@ if ($img) {
 # Create error image if necessary
 if (!$img) {
 	$img = imagecreate(MAX_WIDTH, MAX_HEIGHT);
-	imagecolorallocate($img,0,0,0);
-	$c = imagecolorallocate($img,70,70,70);
-	imageline($img,0,0,MAX_WIDTH,MAX_HEIGHT,$c2);
-	imageline($img,MAX_WIDTH,0,0,MAX_HEIGHT,$c2);
+	imagecolorallocate($img, 0, 0, 0);
+	$c = imagecolorallocate($img, 70, 70, 70);
+	imageline($img, 0, 0, MAX_WIDTH, MAX_HEIGHT, $c2);
+	imageline($img, MAX_WIDTH, 0, 0, MAX_HEIGHT, $c2);
 }
 
 # Display the image
--- thumbnail.php
+++ thumbnail.php
@@ -37,49 +37,51 @@ $image_path = IMAGE_BASE . "/$image_file";
 
 # Load image
 $img = null;
-$ext = strtolower(end(explode('.', $image_path)));
+$temp = explode('.', $image_path);
+$ext = strtolower(end($temp));
 if ($ext == 'jpg' || $ext == 'jpeg') {
-$img = @imagecreatefromjpeg($image_path);
+	$img = @imagecreatefromjpeg($image_path);
 } else if ($ext == 'png') {
-$img = @imagecreatefrompng($image_path);
+	$img = @imagecreatefrompng($image_path);
 # Only if your version of GD includes GIF support
 } else if ($ext == 'gif') {
-$img = @imagecreatefromgif($image_path);
+	$img = @imagecreatefromgif($image_path);
 }
 
 # If an image was successfully loaded, test the image for size
 if ($img) {
 
 # Get image size and scale ratio
-$width = imagesx($img);
-$height = imagesy($img);
-$scale = min(MAX_WIDTH/$width, MAX_HEIGHT/$height);
+	$width = imagesx($img);
+	$height = imagesy($img);
+	$scale = min(MAX_WIDTH / $width, MAX_HEIGHT / $height);
 
 # If the image is larger than the max shrink it
-if ($scale < 1) {
-if (($width > MAX_WIDTH) OR ($height > MAX_HEIGHT)) {
-$new_width = floor($scale*$width);
-$new_height = floor($scale*$height);
+	if ($scale < 1) {
+		if (($width > MAX_WIDTH) OR ($height > MAX_HEIGHT)) {
+			$new_width = floor($scale * $width);
+			$new_height = floor($scale * $height);
 # Create a new temporary image
-$tmp_img = imagecreatetruecolor($new_width, $new_height);
+			$tmp_img = imagecreatetruecolor($new_width, $new_height);
 # Copy and resize old image into new image
-imagecopyresampled($tmp_img, $img, 0, 0, 0, 0,
-$new_width, $new_height, $width, $height);
-imagedestroy($img);
-$img = $tmp_img;
+			imagecopyresampled($tmp_img, $img, 0, 0, 0, 0,
+							   $new_width, $new_height, $width, $height);
+			imagedestroy($img);
+			$img = $tmp_img;
+		}
+	} else {
+		header("Location: $image_path");
+		exit;
+	}
 }
-}else{
-header("Location: $image_path");
-exit;
-}}
 
 # Create error image if necessary
 if (!$img) {
-$img = imagecreate(MAX_WIDTH, MAX_HEIGHT);
-imagecolorallocate($img,0,0,0);
-$c = imagecolorallocate($img,70,70,70);
-imageline($img,0,0,MAX_WIDTH,MAX_HEIGHT,$c2);
-imageline($img,MAX_WIDTH,0,0,MAX_HEIGHT,$c2);
+	$img = imagecreate(MAX_WIDTH, MAX_HEIGHT);
+	imagecolorallocate($img, 0, 0, 0);
+	$c = imagecolorallocate($img, 70, 70, 70);
+	imageline($img, 0, 0, MAX_WIDTH, MAX_HEIGHT, $c2);
+	imageline($img, MAX_WIDTH, 0, 0, MAX_HEIGHT, $c2);
 }
 
 # Display the image
