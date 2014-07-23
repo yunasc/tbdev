cc0ed0009ca22023d6a9f8d7d3c1b913c755294c
VictoRD11@ukr.net <VictoRD11@ukr.net>
2014-07-23 23:53:07 +0300

    fix Memcache::set(flag = 0) delete serialize

--- include/cache.php
+++ include/cache.php
@@ -288,7 +288,7 @@ class Cache_Memcache extends Cache_Common {
 	 * @return bool
 	 */
 	function set($name, $value, $ttl = 0) {
-		return $this->_memcache->set($name, serialize($value), 0, $ttl);
+		return $this->_memcache->set($name, $value, 0, $ttl);
 	}
 
 	/**
